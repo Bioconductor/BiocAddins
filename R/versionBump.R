ce236993421214d2fc119665d7b9d0895bfc7518
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-09-08 16:30:13 +0000

    update versionBump to include auto-commit of DESCRIPTION file

--- R/versionBump.R
+++ R/versionBump.R
@@ -3,6 +3,9 @@
 #' Take the packages version number (x.y.z) and bump the `z` version of the
 #' package. This is also known as the 'patch' version.
 #'
+#' @details Set the option `options('BiocAddins.autocommit')` to `FALSE` to
+#'   avoid automatic `DESCRIPTION` file commits.
+#'
 #' @inheritParams runBiocCheck
 #'
 #' @examples
@@ -13,9 +16,18 @@
 #'
 #' @importFrom desc desc
 #' @export
-bumpVersion <- function(pkgDir = ".") {
+bumpVersion <-
+    function(pkgDir = ".", commit = getOption('BiocAddins.autocommit', TRUE))
+{
     pkgPath <- devtools::as.package(pkgDir)[["path"]]
     desc <- desc::desc(file = file.path(pkgPath, "DESCRIPTION"))
     desc$bump_version("patch")
     desc$write()
+    if (commit) {
+        system2("git", "add DESCRIPTION")
+        system2("git",
+            args = paste0("commit -m 'version bump ",
+                as.character(desc$get_version()), "'")
+        )
+    }
 }
