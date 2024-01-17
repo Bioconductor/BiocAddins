5ef19012a963e9f9896ec190d48072c69c38197c
LiNk-NY <marcel.ramos@roswellpark.org>
2024-01-17 11:38:24 -0500

    add Date to DESC with versionBump action

--- R/versionBump.R
+++ R/versionBump.R
@@ -1,7 +1,8 @@
 #' Bump the current package's patch version
 #'
 #' Take the packages version number (x.y.z) and bump the `z` version of the
-#' package. This is also known as the 'patch' version.
+#' package. This is also known as the 'patch' version. Note this function
+#' also updates the `Date` field in the `DESCRIPTION` file.
 #'
 #' @details Set the option `options('BiocAddins.autocommit')` to `FALSE` to
 #'   avoid automatic `DESCRIPTION` file commits.
@@ -22,6 +23,7 @@ bumpVersion <-
     pkgPath <- devtools::as.package(pkgDir)[["path"]]
     desc <- desc::desc(file = file.path(pkgPath, "DESCRIPTION"))
     desc$bump_version("patch")
+    desc$set("Date", Sys.Date())
     desc$write()
     if (commit) {
         system2("git", "add DESCRIPTION")
